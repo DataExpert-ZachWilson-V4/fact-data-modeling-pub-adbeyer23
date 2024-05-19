b6df0cb14c5c659fce099f0ed0b69e3c14f82b1b
Aaron Beyer <aarbeyer@gmail.com>
2024-05-19 12:13:52 -0400

    Update query_7.sql
    dummy change to retrigger

--- submission/query_7.sql
+++ submission/query_7.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS adbeyer.host_activity_reduced
+ CREATE TABLE IF NOT EXISTS adbeyer.host_activity_reduced
 (host varchar,
 metric_name varchar,
 metric_array array(integer),
